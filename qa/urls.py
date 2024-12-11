eb64672441c5e2da36264f3b4eadc5faed04eaf5
Abhinav kar <abhinav.kar@vvdntech.in>
2024-12-11 14:51:46 +0530

    Added-Views.py

--- qa/urls.py
+++ qa/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.permissions import IsAuthenticated
 
 urlpatterns = [
-    path('upload/', views.upload_files, name='upload_files'),
-]
+    path('admin/upload/pdf/', views.AdminPdfUpload.as_view(), name='pdf_upload'),
+    path('admin/upload/question_image/', views.UserUploadAnswer.as_view(), name='answer_upload'),
+    
+    ]
