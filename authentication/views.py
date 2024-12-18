1888315c7ec02ac2dbac001db186f33910fd19a4
Abhinavkar <abhinav.kar@vvdntech.in>
2024-12-19 00:41:17 +0530

    Added-Login-Refactor

--- authentication/views.py
+++ authentication/views.py
@@ -1,70 +1,62 @@
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializers import RegisterUserSerializer, LoginUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
-from authentication.db_wrapper import get_collection
 from django.contrib.auth.hashers import check_password, make_password
+from .models import User
 
 class RegisterAdminUserView(APIView):
     def post(self, request):
         data = request.data
-        users_collection = get_collection("auth_users")
-        if users_collection.find_one({"username": data["username"]}):
+        if User.objects.filter(username=data["username"]).exists():
             return Response({"error": "Admin already exists"}, status=400)
         hashed_password = make_password(data["password"])
-        users_collection.insert_one({
-            "username": data["username"],
-            "password": hashed_password,  
-            "is_admin": True,
-            "is_superstaff": True,
-        })
+        user = User.objects.create(
+            username=data["username"],
+            password=hashed_password,
+            is_admin=True,
+            is_superuser=True,
+            is_staff=True
+        )
+        user.save()
         return Response({"message": "Admin registered successfully"}, status=201)
 
 
 class RegisterNormalUserView(APIView):
-            
     def post(self, request):
         data = request.data
-        users_collection = get_collection("auth_users")
-        if users_collection.find_one({"username": data["username"]}):
+        if User.objects.filter(username=data["username"]).exists():
             return Response({"error": "User already exists"}, status=400)
         hashed_password = make_password(data["password"])
-        users_collection.insert_one({
-            "username": data["username"],
-            "password": hashed_password,
-            "is_admin": False,
-            "is_superstaff": False,})
-        return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
-
+        user = User.objects.create(
+            username=data["username"],
+            password=hashed_password,
+            is_admin=False,
+            is_superuser=False,
+            is_staff=False
+        )
+        user.save()
+        return Response({"message": "User registered successfully"}, status=201)
 
 
 class LoginUserView(APIView):
     def post(self, request):
         data = request.data
-        users_collection = get_collection("auth_users")
-        print(users_collection)
-
-        # Find user in MongoDB
-        user = users_collection.find_one({"username": data["username"]})
-        print(user)
-
-        if user and check_password(data["password"], user["password"]):  # Compare hashed passwords
-                refresh = RefreshToken()
-                access_token = refresh.access_token
-
-                return Response({
-                    "message": "Admin login successful",
-                    "is_admin": user["is_admin"],
-                    "refresh": str(refresh),
-                    "access": str(access_token),
-                }, status=200)
+        user = authenticate(username=data["username"], password=data["password"])
+        if user is not None:
+            login(request, user)
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "message": "Login successful",
+                "is_admin": user.is_admin,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }, status=200)
         else:
-                return Response({"message": "Invalid credentials"}, status=400)
+            return Response({"message": "Invalid credentials"}, status=400)
 
 
 @permission_classes([IsAuthenticated])
