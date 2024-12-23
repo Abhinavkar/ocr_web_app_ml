13c7f6934a4ec4efb8ef79e8f917c9ef591b9cfe
diganta mohanty <diganta.mohanty@vvdntech.in>
2024-12-23 18:02:29 +0530

    Made changes in apis

--- qa/urls.py
+++ qa/urls.py
@@ -5,14 +5,13 @@ from . import views
 urlpatterns = [
     path('admin/upload/pdf/', views.AdminPdfUpload.as_view(), name='pdf_upload'),
     path('admin/upload/question_image/', views.UserUploadAnswer.as_view(), name='answer_upload'),
-    # path('admin/upload/pdf/list/', views.AdminPdfUpload.as_view(), name='pdf_list'),
     path('admin/upload/pdf/list/',views.AdminPdfGetUpload.as_view(), name='pdf_list'),
 
     # New Class and Subject APIs
     path('classes/', views.ClassListCreateAPI.as_view(), name='class_api'),
     path('subjects/', views.SubjectListCreateAPI.as_view(), name='subject_api'),
     path('subjects/<str:id>/', views.SubjectListCreateAPI.as_view(), name='subject_api'),
-    path('user/upload/answer/pdf/',views.AnswerUploadAPI.as_view(),name="answer_uploadS")
+    path('user/upload/answer/pdf/',views.AnswerUploadAPI.as_view(),name="answer_upload")
 
 
     ]
