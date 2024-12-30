64aae302bbe9f873b6b9e13a1dbe90faabe7751b
diganta mohanty <diganta.mohanty@vvdntech.in>
2024-12-30 16:29:20 +0530

    Created a get API for User_management_operations to get the details user of specific org

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from .views import User_Management_Operations
 urlpatterns = [
     path('users/', User_Management_Operations.as_view(), name='user-list'),
     path('users/<str:id>/', User_Management_Operations.as_view(), name='user-detail'),
+    
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,8 @@ from authentication.permissions import IsAdmin, IsSubAdmin, IsSuperStaff
 from rest_framework.views import APIView
 from rest_framework import status
 from bson import ObjectId
+import json
+from django.http import JsonResponse
 
 class User_Management_Operations(APIView):
     
@@ -21,12 +23,37 @@ class User_Management_Operations(APIView):
     user_collection = get_collection('auth_users')
     org_collection = get_collection('organization_db')
 
+    # def get(self, request):
+    #     users = list(self.user_collection.find())
+    #     for user in users:
+    #         user['_id'] = str(user['_id']) 
+    #     return Response(users, status=status.HTTP_200_OK)
+    
     def get(self, request):
-        users = list(self.user_collection.find())
-        for user in users:
-            user['_id'] = str(user['_id']) 
-        return Response(users, status=status.HTTP_200_OK)
-
+        user_header = request.headers.get('user')  # Get the user header
+        if not user_header:
+            return Response({"error": "User data is required in the headers"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            # Parse the 'user' header as JSON
+            user_header = json.loads(user_header)
+            org_id = user_header.get('organization_id')  # Get the organization_id from the header
+        except json.JSONDecodeError:
+            return Response({"error": "Invalid user header format"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not org_id:
+            return Response({"error": "User does not belong to any organization"}, status=status.HTTP_400_BAD_REQUEST)
+      
+        users_in_org = list(self.user_collection.find({"organization": org_id}))
+  
+        if not users_in_org:
+            return Response({"error": "No users found in the specified organization"}, status=status.HTTP_404_NOT_FOUND)
+        for user in users_in_org:
+            user['_id'] = str(user['_id'])  
+        return Response(users_in_org, status=status.HTTP_200_OK)
+    
+    
+    
     def put(self, request, id):
         if not request.user or not request.user.is_authenticated:
             return Response({"error": "Authentication required"}, status=status.HTTP_401_UNAUTHORIZED)
