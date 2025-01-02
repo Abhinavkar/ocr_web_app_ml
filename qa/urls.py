fbd5a654280268dae971db54168106ed9ce70b1c
Abhinavkar <abhinav.kar@vvdntech.in>
2025-01-02 16:18:24 +0530

    Added-API

--- qa/urls.py
+++ qa/urls.py
@@ -5,7 +5,6 @@ from .views import AdminPdfDeleteUpload, AdminQuestionUpload, ResultRetrieveAPI
 
 urlpatterns = [
     path('admin/upload/pdf/', views.AdminPdfUpload.as_view(), name='pdf_upload'),
-    path('admin/upload/question_image/', views.UserUploadAnswer.as_view(), name='answer_upload'),
     path('admin/upload/pdf/list/',views.AdminPdfGetUpload.as_view(), name='pdf_list'),
     # New Class and Subject APIs
     path('user/upload/answer/pdf/',views.AnswerUploadAPI.as_view(),name="answer_upload"),
